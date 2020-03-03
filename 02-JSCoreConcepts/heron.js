8a162aa539ece0d1070f9dcea0f03365decc4359
s-115-19 <s-115-19@gumiforest.ae.krakow.pl>
2020-03-03 15:37:26 +0100

    my second commit :)

--- 02-JSCoreConcepts/heron.js
+++ 02-JSCoreConcepts/heron.js
@@ -1,11 +1,15 @@
 // boki trójkąta
-//a = 3;
+a = 3;
 b = 4;
 c = 5;
 h = 7
 
 // Pole trókąta o bokach ..., ... oraz ... wynosi ... .
 
-let a = () => "aaaa";
+let triangle_area = (a, b, c) => {
+    p = 0.5 * (a + b + c)
+    result = Math.sqrt(p*(p-a)*(p-b)*(p-c))
+    return `Pole trojkata o bokach ${a}, ${b} oraz ${c} wynosi ${result}`
+}
 
-console.log( a() );
+console.log(triangle_area(a, b, c));
